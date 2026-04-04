7e7dc12f00a2b151b2f30f8b511ce9919369b4a5
Shruti Patnaik <patnaik.shruti123@gmail.com>
2026-04-04 15:32:21 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,55 @@
+const express=require("express");
+const router=express.Router();
+const wrapAsync=require("../utils/wrapAsync.js");
+const Listing=require("../models/listing.js");
+const {isLoggedIn, isOwner,validateListing}=require("../middleware.js");
+
+const listingController=require("../controllers/listings.js");
+const multer=require('multer');
+const {storage}=require("../cloudConfig.js");
+const upload=multer({storage});
+
+
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+.post(   
+     isLoggedIn,
+     upload.single('listing[image]'),  
+     validateListing,
+    wrapAsync(listingController.createListing)
+);
+
+
+//New Route
+    router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+router.route("/:id")
+.get(
+    wrapAsync(listingController.showListing)
+)
+.put
+(
+    isLoggedIn,  //pehle check krenge ki kya user logged in h
+    isOwner,
+    upload.single('listing[image]'),  
+    validateListing,  //fir dekhenge ki kya us user k paas permission h loggin krne ki
+    wrapAsync(listingController.updateListing)
+)
+.delete(
+    
+     isLoggedIn,
+     isOwner,
+     wrapAsync(listingController.destroyListing));
+
+
+
+//EDIT ROUTE
+router.get(
+    "/:id/edit",
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.renderEditForm));
+
+
+module.exports=router;
